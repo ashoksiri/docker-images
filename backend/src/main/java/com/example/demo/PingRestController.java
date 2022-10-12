57f5cf0fa8f4e9f7a557d1819b811f7f137522b9
ashok <Ashok.Siripuram1@T-Mobile.com>
2022-10-13 00:13:29 +0530

    changed to war

--- backend/src/main/java/com/example/demo/PingRestController.java
+++ backend/src/main/java/com/example/demo/PingRestController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 public class PingRestController {
@@ -11,6 +13,13 @@ public class PingRestController {
   @RequestMapping(method = RequestMethod.GET, path = "/api/ping")
   public ResponseEntity<String> getPing() {
     return ResponseEntity.ok("pong");
+  }
+
+  @RequestMapping(method = RequestMethod.GET, path = "/api/welcome")
+  public ResponseEntity<Map<String, String>> wishMe() {
+    Map<String, String> resp = new HashMap<>();
+    resp.put("welcome", "ashok");
+    return ResponseEntity.ok(resp);
   }
 
 }
